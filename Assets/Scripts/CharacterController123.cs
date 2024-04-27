84b59a9346695dfe8b762d804f6b993934764c0d
Furkan Bahar 45 <mfbahartr@gmail.com>
2024-04-27 11:57:06 +0300

    character

---
+++ Assets/Scripts/CharacterController123.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController123 : MonoBehaviour
+{
+    Rigidbody2D charphysic;
+    public float Boost = 1f;
+    bool facingright = true;
+    Animator animations;
+    // Start is called before the first frame update
+    void Start()
+    {
+        charphysic = GetComponent<Rigidbody2D>();
+        animations = GetComponent<Animator>();
+    }
+    void Awake()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Horizontalmove();
+        if (charphysic.velocity.x < 0 && facingright) 
+        {
+            FlipFace();
+        }  
+        else if (charphysic.velocity.x >0 && !facingright)
+        {
+            FlipFace();
+        }
+
+    }
+    void Horizontalmove()
+    {
+        charphysic.velocity = new Vector2(Input.GetAxis("Horizontal") * Boost, charphysic.velocity.y);
+        animations.SetFloat("CharSpeed", Mathf.Abs(charphysic.velocity.x));
+    }
+    void FixedUpdate()
+    {
+        
+    }
+    void FlipFace()
+    {
+        facingright = !facingright;
+        Vector3 TempLocScal = transform.localScale;
+        TempLocScal.x *= -1;
+        transform.localScale = TempLocScal;
+    }
+}
