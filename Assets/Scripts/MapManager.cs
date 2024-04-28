9dd0773b8441747187f2a356828be3a613460c4c
tiarlow <baran06senol@gmail.com>
2024-04-28 15:10:06 +0300

    random map

---
+++ Assets/Scripts/MapManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+public class MapManager : MonoBehaviour
+{
+    public GameObject startMap;
+    public List<GameObject> maps;
+
+    private GameObject currentMap;
+    private Transform spawnPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        loadMap();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void loadMap()
+    {
+        if (spawnPoint == null)
+        {
+            spawnPoint = startMap.transform.GetChild(1).gameObject.transform;
+
+        } else
+        {
+            spawnPoint = currentMap.transform.GetChild(1).gameObject.transform;
+        }
+        
+        generateRandomMap();
+        Instantiate(currentMap, spawnPoint.position, spawnPoint.rotation);
+    }
+
+    void generateRandomMap()
+    {
+        var random = new System.Random();
+        int randomIndex = random.Next(0, maps.Count);
+
+        currentMap = maps[randomIndex];
+    }
+}
