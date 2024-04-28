297bb1be8e4f1b6c43c466f93963f6c8ed16ac58
Furkan Bahar 45 <mfbahartr@gmail.com>
2024-04-28 16:47:41 +0300

    kapitalizm

---
+++ Assets/Scripts/AppleManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleManager : MonoBehaviour
+{
+    Animator animations;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animations = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        animations.SetBool("is_touched",true);
+        
+    }
+    void Destroy1()
+    {
+        Destroy(this);
+    }
+}
